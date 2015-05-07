2ae2cd631f4d54e03615faa07fc4333f61776b3c
cliu <fluency.03@gmail.com>
2015-05-07 20:25:15 +0200

    Best Time to Buy and Sell Stock IV

---
+++ best_time_to_buy_and_sell_stock_IV.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# Say you have an array for which the ith element is the price of a given stock on day i.
+
+# Design an algorithm to find the maximum profit. You may complete at most k transactions.
+
+# Note:
+# You may not engage in multiple transactions at the same time (i.e., you must sell the 
+# stock before you buy again).
+
+class Solution:
+	# @param {integer} k
+	# @param {integer[]} prices
+	# @return {integer}
+	def maxProfit(self, k, prices):
