d8cee2c452fb3dae2172e30a189a693ad827809f
cliu <fluency.03@gmail.com>
2015-05-09 13:53:55 +0200

    min coins

--- min_coins_for_amount_s.py
+++ min_coins_for_amount_s.py
@@ -16,6 +16,22 @@ class Solution:
 				if ( coins[j]<=i and Min[i-coins[j]]<Min[i] ):
 					Min[i] = Min[i-coins[j]]+1
 					choice[i] = choice[i-coins[j]] + [coins[j]]
+		print (Min)
+		print (choice)
+		if Min[s] == float('inf'):
+			print ("Not possible!!!")
+		else:
+			return Min[s]
+	def minCoins1(self, coins, s):
+		Min = [float('inf')] * (s+1)
+		Min[0] = 0
+		choice = [[]] * (s+1)
+		for j in range(len(coins)):
+			for i in range(0, s+1):
+				if (i+coins[j]) <= s and Min[i]<Min[i+coins[j]]:
+					Min[i+coins[j]] = Min[i]+1
+					choice[i+coins[j]] = choice[i] + [coins[j]]
+		print (Min)
 		print (choice)
 		if Min[s] == float('inf'):
 			print ("Not possible!!!")
@@ -23,10 +39,13 @@ class Solution:
 			return Min[s]
 
 
+
+
+
 solution = Solution()
 
 a = [2, 3, 5]
-s = 1
+s = 10
 
 print (solution.minCoins(a, s))
 
