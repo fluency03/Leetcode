e0656dd89f7057b2473280cd4a5a7129cc0e0d68
cliu <fluency.03@gmail.com>
2015-05-10 20:46:45 +0200

    compare to key

---
+++ cmp_to_key.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# This def is used to handle cmp to key, since cmp cannot be used in Python3
+# For instance, l.sort(cmp=mycmp) converts to l.sort(key=cmp_to_key(mycmp)).
+
+def cmp_to_key(mycmp):
+    'Convert a cmp= function into a key= function'
+    class K(object):
+        def __init__(self, obj, *args):
+            self.obj = obj
+        def __lt__(self, other):
+            return mycmp(self.obj, other.obj) < 0
+        def __gt__(self, other):
+            return mycmp(self.obj, other.obj) > 0
+        def __eq__(self, other):
+            return mycmp(self.obj, other.obj) == 0
+        def __le__(self, other):
+            return mycmp(self.obj, other.obj) <= 0  
+        def __ge__(self, other):
+            return mycmp(self.obj, other.obj) >= 0
+        def __ne__(self, other):
+            return mycmp(self.obj, other.obj) != 0
+    return K
